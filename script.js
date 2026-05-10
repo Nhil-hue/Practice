83e1fa1af48599f74cdcb1933d0b853ef71544d2
Nhil <221099407+Nhil-hue@users.noreply.github.com>
2026-05-10 08:10:17 +0800

    getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+function getComputerChoice(){
+    let random = Math.random();
+
+    if(random < 0.33){
+        return "rock";
+    }
+    else if (random < 0.66){
+        return "paper";
+    }else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
